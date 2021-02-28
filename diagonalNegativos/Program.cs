b1b442b4fc542c03c6b435bfd8acddcc6530e981
Eduardo Façanha <59966140+eduardofacf@users.noreply.github.com>
2021-02-28 11:32:39 -0300

    Add files via upload

---
+++ diagonalNegativos/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace diagonalNegativos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n, cont;
+
+            Console.Write("Qual a ordem da matriz? ");
+            n = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write("Elemento [" + i + "," + j + "]: ");
+                    mat[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("DIAGONAL PRINCIPAL:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(mat[i, i] + " ");
+            }
+
+            cont = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if ( mat[i, j] < 0)
+                    {
+                        cont++;
+                    }
+                }
+            }
+            
+            Console.WriteLine();
+            Console.WriteLine("QUANTIDADE DE NEGATIVOS = " + cont);
+
+        }
+    }
+}
